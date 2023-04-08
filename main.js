981c60bf033579479165a96531be070f29ac366d
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-04-08 10:09:39 +0300

    добавлена валидация email, просмотр пароля

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const dlg_body = document.getElementsByClassName('dlg_body');
+const open_dlg = document.getElementById('open_dlg');
+const full_modal = document.getElementById('my_modal');
+const eyes = document.querySelector('.eyes');
+const inputpswd = document.querySelector('#pswd');
+const error = document.querySelector('#error')
+
 document.getElementById("open_dlg").addEventListener("click", function(){
     document.getElementById("my_modal").classList.add("open")
 });
@@ -6,11 +13,33 @@ document.getElementById("close_dlg").addEventListener("click", function(){
     document.getElementById("my_modal").classList.remove("open")
 });
 
-documen.querySelector("#my_modal .dlg_modal").addEventListener('click', event => {
-    event._isClickWithInModal = true;
-});
+full_modal.addEventListener('click', (e) => {
+	if(e.target.id === "my_modal" && e.target.id != "open_dlg") {
+        document.getElementById("my_modal").classList.remove("open")
+    }
+})
 
-document.getElementById("my_modal").addEventListener("click", event => {
-    if(event._isClickWithInModal) return;
-    event.currentTarget.classList.remove('open');
-});
+// eyes.onfocus = (e) => inputpswd.type = 'text';
+// eyes.onblure = (e) => inputpswd.type = 'password';
+eyes.addEventListener('pointerdown', ()=> {
+    inputpswd.setAttribute('type', 'text');
+})
+
+eyes.addEventListener('pointerup', ()=> {
+    inputpswd.setAttribute('type', 'password');
+})
+
+
+mail.onblur = function() {
+    if (!mail.value.includes('@')) {
+      mail.classList.add('invalid');
+      error.innerHTML = 'Пожалуйста, введите правильный email.'
+    }
+  };
+  
+  mail.onfocus = function() {
+    if (this.classList.contains('invalid')) {
+      this.classList.remove('invalid');
+      error.innerHTML = "";
+    }
+  };
